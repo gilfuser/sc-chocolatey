b2fa91d9c10e181dfcae00b67d077f9a71eacb2b
Gil Fuser <gilfuser@gmail.com>
2026-08-23 19:06:23 -0300

    Update SuperCollider uninstall metadata for 3.14.1

--- supercollider/tools/chocolateyuninstall.ps1
+++ supercollider/tools/chocolateyuninstall.ps1
@@ -1,35 +1,25 @@
-$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop'
 
 $packageName = 'supercollider'
-$softwareName = 'SuperCollider Version 3.12.1'
-$installerType = 'EXE' 
-
+$softwareName = 'SuperCollider Version 3.14.1'
+$installerType = 'EXE'
 $silentArgs = '/S'
 $validExitCodes = @(0)
-$uninstalled = $false
 
-[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+[array]$keys = Get-UninstallRegistryKey -SoftwareName $softwareName
 
-if ($key.Count -eq 1) {
-  $key | % { 
+if ($keys.Count -eq 1) {
+  $keys | ForEach-Object {
     $file = "$($_.UninstallString)"
-
-    if ($installerType -eq 'MSI') {
-      $silentArgs = "$($_.PSChildName) $silentArgs"
-      $file = ''
-    }
-
     Uninstall-ChocolateyPackage -PackageName $packageName `
-                                -FileType $installerType `
-                                -SilentArgs "$silentArgs" `
-                                -ValidExitCodes $validExitCodes `
-                                -File "$file"
+      -FileType $installerType `
+      -SilentArgs $silentArgs `
+      -ValidExitCodes $validExitCodes `
+      -File $file
   }
-} elseif ($key.Count -eq 0) {
+} elseif ($keys.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
-  Write-Warning "$key.Count matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $_.DisplayName"}
+} else {
+  Write-Warning "$($keys.Count) matching uninstall entries found; refusing to guess."
+  $keys | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
 }
