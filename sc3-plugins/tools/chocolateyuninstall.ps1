630d7d16c8d673c9c52dabdebc70fb5a58e75a29
Gil Fuser <gilfuser@gmail.com>
2026-08-23 21:33:15 -0300

    Harden sc3-plugins uninstall cleanup

--- sc3-plugins/tools/chocolateyuninstall.ps1
+++ sc3-plugins/tools/chocolateyuninstall.ps1
@@ -1,9 +1,19 @@
-$scExtensionsPath = $env:LOCALAPPDATA + '\SuperCollider\Extensions'
-$scPluginsPath = $scExtensionsPath + '\SC3plugins'
+$ErrorActionPreference = 'Stop'
 
-Write-Host 'Checking for path' $scPluginsPath
+$pluginsDir = Join-Path $env:LOCALAPPDATA 'SuperCollider\Extensions\SC3plugins'
+$legacyInstallDir = Join-Path $env:LOCALAPPDATA 'SuperCollider\Extensions\install'
+$legacyPluginsDir = Join-Path $legacyInstallDir 'SC3plugins'
 
-if(Test-Path -Path $scPluginsPath){
-    Write-Host 'Removing ' $scPluginsPath
-    Remove-Item $scPluginsPath -force -recurse
+if (Test-Path $pluginsDir) {
+  Write-Host "Removing $pluginsDir"
+  Remove-Item -Path $pluginsDir -Recurse -Force
+}
+
+if (Test-Path $legacyPluginsDir) {
+  Write-Host "Removing legacy nested $legacyPluginsDir"
+  Remove-Item -Path $legacyPluginsDir -Recurse -Force
+}
+
+if ((Test-Path $legacyInstallDir) -and -not (Get-ChildItem -Path $legacyInstallDir -Force -ErrorAction SilentlyContinue)) {
+  Remove-Item -Path $legacyInstallDir -Force
 }
