8ac72f024566dfb001da2f8a73a48215d4d57fc3
Gil Fuser <gilfuser@gmail.com>
2026-08-23 22:06:57 -0300

    Use pinned SuperCollider release URL and checksum

--- supercollider/tools/chocolateyinstall.ps1
+++ supercollider/tools/chocolateyinstall.ps1
@@ -6,30 +6,14 @@ if (-not [Environment]::Is64BitOperatingSystem) {
 
 $packageName = 'supercollider'
 $version = '3.14.1'
-$assetName = 'SuperCollider-3.14.1_Release-x64-VS-426edf6.exe'
-$releaseApi = "https://api.github.com/repos/supercollider/supercollider/releases/tags/Version-$version"
-
-Write-Host "Resolving official SuperCollider $version release asset..."
-$headers = @{ 'User-Agent' = 'sc-chocolatey' }
-$release = Invoke-RestMethod -Uri $releaseApi -Headers $headers
-$asset = $release.assets | Where-Object { $_.name -eq $assetName } | Select-Object -First 1
-
-if (-not $asset) {
-  throw "Could not find $assetName in SuperCollider release Version-$version."
-}
-
-if (-not $asset.digest -or $asset.digest -notmatch '^sha256:([0-9a-fA-F]{64})$') {
-  throw "GitHub did not provide a valid SHA-256 digest for $assetName."
-}
-
-$checksum = $Matches[1]
+$url = 'https://github.com/supercollider/supercollider/releases/download/Version-3.14.1/SuperCollider-3.14.1_Release-x64-VS-426edf6.exe'
 
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'EXE'
-  url            = $asset.browser_download_url
+  url            = $url
   softwareName   = "SuperCollider Version $version"
-  checksum       = $checksum
+  checksum       = 'EBB27352978539AD5D0E8BB6707CEE10C3C59ABCD9ACB0C2B2D6221081396F30'
   checksumType   = 'sha256'
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/S'
